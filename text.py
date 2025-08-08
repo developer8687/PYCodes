eed5e33a2787a3486eeedf980361f17db4a36288
hasan <developer8687@gmail.com>
2025-08-08 19:32:26 +0530

    fix

---
+++ text.py
@@ -0,0 +1,22 @@
+# a = 5
+# b = 'hasan1'
+# c=4/2
+# print(a+2+3)
+
+# print(type(b))
+
+# print(type(c))
+# print(c)
+
+# x = "55"
+# print(type(x))
+# y=int(x)
+# print(type(y))
+score3 =int(input())
+
+
+score = int(input("Enter your score: "))
+score2 = int(input("Enter your score2: "))
+results = score + score2
+print(score+score2)
+print("totalscore:", results) ok va ok will connect later
